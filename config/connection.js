d653ec83d11826bb2887206eae3a57c28c6d2450
anahisbarrie <anahis.barrie@gmail.com>
2019-08-24 13:11:32 -0400

    fixing minor bugs, still need to check views

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ var mysql = require('mysql');
 
 var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// }
+// else {
     connection = mysql.createConnection(
         {
             host: 'localhost',
@@ -15,7 +15,7 @@ else {
             password: 'mango2019',
             database: 'burgers_db'
         });
-};
+// };
 
 
 connection.connect(function (err) {
